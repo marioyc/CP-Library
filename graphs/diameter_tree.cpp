bed713cfdc33cf6984589227fa3802a1f5915ea0
Mario Ynocente Castro <ycmario@gmail.com>
2025-08-27 11:25:37 +0900

    update diameter of tree

--- graphs/diameter_tree.cpp
+++ graphs/diameter_tree.cpp
@@ -1,31 +1,60 @@
-#define MAX_SIZE 100
-bool visited[MAX_SIZE];
-int prev[MAX_SIZE];
+// Diameter of a tree: https://codeforces.com/blog/entry/101271
+// sample submission: https://codeforces.com/contest/2134/submission/335741610
 
-int most_distant(int s){
-    queue<int> Q;
-    Q.push(s);
-    
-    memset(visited,false,sizeof(visited));
-    visited[s] = true;
-    prev[s] = -1;
-    
-    int ans = s;
-    
-    while(!Q.empty()){
-        int aux = Q.front();
-        Q.pop();
-        
-        ans = aux;
-        
-        for(int i=L[aux].size()-1;i>=0;--i){
-            int v = L[aux][i];
-            if(visited[v]) continue;
-            visited[v] = true;
-            Q.push(v);
-            prev[v] = aux;
-        }
+struct TreeDiameter{
+    int n;
+    vector< vector<int> > L;
+    vector<bool> visited;
+    vector<int> prev;
+
+    TreeDiameter(int _n){
+        n = _n;
+        L = vector< vector<int> >(n);
+        visited = vector<bool>(n);
+        prev = vector<int>(n);
     }
-    
-    return ans;
-}
+
+    void add_edge(int u, int v){
+        assert(u >= 0 && u < n);
+        assert(v >= 0 && v < n);
+        L[u].push_back(v);
+        L[v].push_back(u);
+    }
+
+    int most_distant(int s){
+        fill(visited.begin(), visited.end(), false);
+        queue<int> Q;
+        Q.push(s);
+        visited[s] = true;
+        prev[s] = -1;
+        int ans = s;
+
+        while(!Q.empty()){
+            int cur = Q.front();
+            Q.pop();
+            ans = cur;
+
+            for(int to : L[cur]){
+                if(!visited[to]){
+                    visited[to] = true;
+                    Q.push(to);
+                    prev[to] = cur;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    vector<int> get_diameter(){
+        int s = most_distant(0);
+        int e = most_distant(s);
+        vector<int> ret;
+        while(e != s){
+            ret.push_back(e);
+            e = prev[e];
+        }
+        ret.push_back(s);
+        return ret;
+    }
+};
